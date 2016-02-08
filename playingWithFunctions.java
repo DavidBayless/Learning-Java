e5b818099b2283bd0d480930002f3c6285c696e7
David Bayless <David.C.Bayless15@gmail.com>
2016-02-08 15:29:10 -0700

    debugging

--- playingWithFunctions.java
+++ playingWithFunctions.java
@@ -13,3 +13,29 @@ class FunctionTesting {
     System.out.println("Bar");
   }
 }
+
+class Kata {
+  public static int rentalCarCost(int d) {
+    // Your solution here
+    int total = 40;
+    total *= d;
+    if (d > 6) {
+      total -= 50;
+    } else if (d > 2) {
+      total -= 20;
+    }
+    System.out.println(total);
+    return total;
+  }
+
+  public static void main( String[]args ) {
+    rentalCarCost(1);
+    rentalCarCost(2);
+    rentalCarCost(3);
+    rentalCarCost(6);
+    rentalCarCost(7);
+    rentalCarCost(9);
+
+
+  }
+}
