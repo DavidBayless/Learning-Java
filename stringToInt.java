3767d52ab24a284e6aa5b42d9d0b5c2be46e0974
David Bayless <David.C.Bayless15@gmail.com>
2016-02-12 16:21:39 -0700

    made type conversion using java functions

---
+++ stringToInt.java
@@ -0,0 +1,8 @@
+class StringToInt {
+  public static void main( String[]args ) {
+    String numberString = "123";
+    int number = Integer.parseInt(gnumberString);
+    number += 321;
+    System.out.println(number);
+  }
+}
