f01793d65603526acb69a457fc3231e44e59f0b6
David Bayless <David.C.Bayless15@gmail.com>
2016-02-14 09:30:40 -0700

    added window with buttons

---
+++ guiPractice.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+class Window extends JFrame{
+  JPanel pnl = new JPanel();
+  ImageIcon tick = new ImageIcon( "tick.png" );
+  ImageIcon cross = new ImageIcon( "cross.png");
+
+  public Window() {
+    super( "Swing Window");
+    setSize( 500, 200 );
+    setDefaultCloseOperation( EXIT_ON_CLOSE );
+    add( pnl );
+    setVisible( true );
+    JButton btn = new JButton( "Click Me");
+    JButton tickBtn = new JButton( "GO");
+    JButton crossBtn = new JButton( "STOP");
+    pnl.add( btn );
+    pnl.add( tickBtn );
+    pnl.add( crossBtn );
+  }
+
+  public static void main( String[]args ) {
+    Window gui = new Window();
+  }
+}
