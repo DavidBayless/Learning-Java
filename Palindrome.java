7b581338950dbc1ed9ea101c1d61db5899662dd1
David Bayless <David.C.Bayless15@gmail.com>
2016-03-08 09:19:27 -0700

    updated palindrom to be accurate with spaces

--- Palindrome.java
+++ Palindrome.java
@@ -2,6 +2,10 @@ public class Palindrome {
   public static void main(String [] args) {
     String str = args[0];
     String revStr = new StringBuilder(str).reverse().toString();
+    String [] strArr = str.split(" ", -1);
+    String [] revStrArr = revStr.split(" ", -1);
+    str = String.join("", strArr);
+    revStr = String.join("", revStrArr);
     System.out.println(revStr);
     System.out.println(str);
     if (revStr.equals(str)) {
